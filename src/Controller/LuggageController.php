f05effabbd5da3a2e519b0c9559bf3ac09d29867
Gibon_david <[My\_gibon.david@live.fr](mailto:MY_gibon.david@live.fr)>
2023-05-12 10:16:59 +0200

    function active for 3 pages

--- src/Controller/LuggageController.php
+++ src/Controller/LuggageController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Model\LuggageManager;
+use function Symfony\Component\DependencyInjection\Loader\Configurator\inline_service;
 
 class LuggageController extends AbstractController
 {
@@ -13,20 +14,61 @@ class LuggageController extends AbstractController
 
     public function select()
     {
+
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-            $luggageManager = new LuggageManager();
-            $luggages = $luggageManager->selectByid();
-            return $this->twig->render('Luggage/select.html.twig', ['luggages' => $luggages]);
+            if (isset($_POST['mountain']) && isset($_POST['warm']) && isset($_POST['room'])) {
+                $luggageManager = new LuggageManager();
+                $luggages = $luggageManager->selectByMwr();
+
+            } elseif (isset($_POST['mountain']) && isset($_POST['warm']) && isset($_POST['outside'])) {
+                $luggageManager = new LuggageManager();
+                $luggages = $luggageManager->selectByMwo();
+
+            } elseif (isset($_POST['mountain']) && isset($_POST['cold']) && isset($_POST['outside'])) {
+                $luggageManager = new LuggageManager();
+                $luggages = $luggageManager->selectByMco();
+
+            } elseif (isset($_POST['mountain']) && isset($_POST['cold']) && isset($_POST['room'])) {
+                $luggageManager = new LuggageManager();
+                $luggages = $luggageManager->selectByMcr();
+            }elseif (isset($_POST['beach']) && isset($_POST['cold']) && isset($_POST['room'])) {
+                $luggageManager = new LuggageManager();
+                $luggages = $luggageManager->selectByBcr();
+            }elseif (isset($_POST['beach']) && isset($_POST['warm']) && isset($_POST['outside'])) {
+                $luggageManager = new LuggageManager();
+                $luggages = $luggageManager->selectByBwo();
+            }elseif (isset($_POST['beach']) && isset($_POST['cold']) && isset($_POST['outside'])) {
+                $luggageManager = new LuggageManager();
+                $luggages = $luggageManager->selectByBco();
+            }elseif (isset($_POST['beach']) && isset($_POST['warm']) && isset($_POST['room'])) {
+                $luggageManager = new LuggageManager();
+                $luggages = $luggageManager->selectByBwr();
+            }elseif (isset($_POST['urban']) && isset($_POST['warm']) && isset($_POST['room'])) {
+                $luggageManager = new LuggageManager();
+                $luggages = $luggageManager->selectByUwr();
+            }elseif (isset($_POST['urban']) && isset($_POST['warm']) && isset($_POST['outside'])) {
+                $luggageManager = new LuggageManager();
+                $luggages = $luggageManager->selectByUwo();
+            }elseif (isset($_POST['urban']) && isset($_POST['cold']) && isset($_POST['outside'])) {
+                $luggageManager = new LuggageManager();
+                $luggages = $luggageManager->selectByUco();
+            }elseif (isset($_POST['urban']) && isset($_POST['cold']) && isset($_POST['room'])) {
+                $luggageManager = new LuggageManager();
+                $luggages = $luggageManager->selectByUcr();
+            }
         }
+            return $this->twig->render('Luggage/select.html.twig', ['luggages' => $luggages]);
     }
 
-    public function result()
+
+    public function result() : string
     {
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-            $luggageManager = new LuggageManager();
-            $luggages = $luggageManager->selectForLuggage();
 
-            return $this->twig->render('Luggage/result.html.twig', ['luggages' => $luggages]);
+          if(isset($_POST)){
+              $luggages = $_POST;
+          }
         }
+            return $this->twig->render('Luggage/result.html.twig', ['luggages' => $luggages]);
     }
 }
--- src/Model/LuggageManager.php
+++ src/Model/LuggageManager.php
@@ -11,30 +11,187 @@ class LuggageManager extends AbstractManager
     /**
      * @return array|false
      */
-    public function selectByid()
+    public function selectByMwr()
     {
-        $sql = "SELECT name, image, category FROM item
+        $sql = "SELECT  name, image, category FROM item
                WHERE mountain = :mountain
-                 AND beach = :beach
-                 AND urban = :urban
-                 AND smallTemp = :warm
-                 AND hightTemp = :cold
-                 AND hotel = :room
+                 AND hightTemp = :warm
+                 AND hotel = :room";
+
+        $stm = $this->pdo->prepare($sql);
+        $stm->bindValue('mountain', ':mountain', PDO::PARAM_BOOL);
+        $stm->bindValue('warm', ':warm', PDO::PARAM_BOOL);
+        $stm->bindValue('room', ':room', PDO::PARAM_BOOL);
+
+        $stm->execute();
+        return $stm->fetchAll();
+    }
+    public function selectByMwo()
+    {
+        $sql = "SELECT  name, image, category FROM item
+               WHERE mountain = :mountain
+                 AND hightTemp = :warm
                  AND extern = :outside";
 
         $stm = $this->pdo->prepare($sql);
         $stm->bindValue('mountain', ':mountain', PDO::PARAM_BOOL);
-        $stm->bindValue('beach', ':beach', PDO::PARAM_BOOL);
-        $stm->bindValue('urban', ':urban', PDO::PARAM_BOOL);
         $stm->bindValue('warm', ':warm', PDO::PARAM_BOOL);
+        $stm->bindValue('outside', ':outside', PDO::PARAM_BOOL);
+
+        $stm->execute();
+        return $stm->fetchAll();
+    }
+    public function selectByMco()
+    {
+        $sql = "SELECT  name, image, category FROM item
+               WHERE mountain = :mountain
+                 AND smallTemp = :cold
+                 AND extern = :outside";
+
+        $stm = $this->pdo->prepare($sql);
+        $stm->bindValue('mountain', ':mountain', PDO::PARAM_BOOL);
+        $stm->bindValue('cold', ':cold', PDO::PARAM_BOOL);
+        $stm->bindValue('outside', ':outside', PDO::PARAM_BOOL);
+
+        $stm->execute();
+        return $stm->fetchAll();
+    }
+    public function selectByMcr()
+    {
+        $sql = "SELECT  name, image, category FROM item
+               WHERE mountain = :mountain
+                 AND smallTemp = :cold
+                 AND hotel = :room";
+
+        $stm = $this->pdo->prepare($sql);
+        $stm->bindValue('mountain', ':mountain', PDO::PARAM_BOOL);
         $stm->bindValue('cold', ':cold', PDO::PARAM_BOOL);
         $stm->bindValue('room', ':room', PDO::PARAM_BOOL);
+
+        $stm->execute();
+        return $stm->fetchAll();
+    }
+    public function selectByBcr()
+    {
+        $sql = "SELECT  name, image, category FROM item
+               WHERE beach = :beach
+                 AND smallTemp = :cold
+                 AND hotel = :room";
+
+        $stm = $this->pdo->prepare($sql);
+        $stm->bindValue('beach', ':beach', PDO::PARAM_BOOL);
+        $stm->bindValue('cold', ':cold', PDO::PARAM_BOOL);
+        $stm->bindValue('room', ':room', PDO::PARAM_BOOL);
+
+        $stm->execute();
+        return $stm->fetchAll();
+    }
+    public function selectByBwr()
+    {
+        $sql = "SELECT  name, image, category FROM item
+               WHERE beach = :beach
+                 AND hightTemp = :warm
+                 AND hotel = :room";
+
+        $stm = $this->pdo->prepare($sql);
+        $stm->bindValue('beach', ':beach', PDO::PARAM_BOOL);
+        $stm->bindValue('warm', ':warm', PDO::PARAM_BOOL);
+        $stm->bindValue('room', ':room', PDO::PARAM_BOOL);
+
+        $stm->execute();
+        return $stm->fetchAll();
+    }
+    public function selectByBwo()
+    {
+        $sql = "SELECT  name, image, category FROM item
+               WHERE beach = :beach
+                 AND hightTemp = :warm
+                 AND extern = :outside";
+
+        $stm = $this->pdo->prepare($sql);
+        $stm->bindValue('beach', ':beach', PDO::PARAM_BOOL);
+        $stm->bindValue('warm', ':warm', PDO::PARAM_BOOL);
+        $stm->bindValue('outside', ':outside', PDO::PARAM_BOOL);
+
+        $stm->execute();
+        return $stm->fetchAll();
+    }
+    public function selectByBco()
+    {
+        $sql = "SELECT  name, image, category FROM item
+               WHERE beach = :beach
+                 AND smallTemp = :cold
+                 AND extern = :outside";
+
+        $stm = $this->pdo->prepare($sql);
+        $stm->bindValue('beach', ':beach', PDO::PARAM_BOOL);
+        $stm->bindValue('cold', ':cold', PDO::PARAM_BOOL);
         $stm->bindValue('outside', ':outside', PDO::PARAM_BOOL);
 
         $stm->execute();
         return $stm->fetchAll();
     }
 
+    public function selectByUwr()
+    {
+        $sql = "SELECT  name, image, category FROM item
+               WHERE urban = :urban
+                 AND hightTemp = :warm
+                 AND hotel = :room";
+
+        $stm = $this->pdo->prepare($sql);
+        $stm->bindValue('urban', ':urban', PDO::PARAM_BOOL);
+        $stm->bindValue('warm', ':warm', PDO::PARAM_BOOL);
+        $stm->bindValue('room', ':room', PDO::PARAM_BOOL);
+
+        $stm->execute();
+        return $stm->fetchAll();
+    }
+    public function selectByUwo()
+    {
+        $sql = "SELECT  name, image, category FROM item
+               WHERE urban = :urban
+                 AND hightTemp = :warm
+                 AND extern = :outside";
+
+        $stm = $this->pdo->prepare($sql);
+        $stm->bindValue('urban', ':urban', PDO::PARAM_BOOL);
+        $stm->bindValue('warm', ':warm', PDO::PARAM_BOOL);
+        $stm->bindValue('outside', ':outside', PDO::PARAM_BOOL);
+
+        $stm->execute();
+        return $stm->fetchAll();
+    }
+    public function selectByUco()
+    {
+        $sql = "SELECT  name, image, category FROM item
+               WHERE urban = :urban
+                 AND smallTemp = :cold
+                 AND extern = :outside";
+
+        $stm = $this->pdo->prepare($sql);
+        $stm->bindValue('urban', ':urban', PDO::PARAM_BOOL);
+        $stm->bindValue('cold', ':cold', PDO::PARAM_BOOL);
+        $stm->bindValue('outside', ':outside', PDO::PARAM_BOOL);
+
+        $stm->execute();
+        return $stm->fetchAll();
+    }
+    public function selectByUcr()
+    {
+        $sql = "SELECT  name, image, category FROM item
+               WHERE urban = :urban
+                 AND smallTemp = :cold
+                 AND hotel = :room";
+
+        $stm = $this->pdo->prepare($sql);
+        $stm->bindValue('urban', ':urban', PDO::PARAM_BOOL);
+        $stm->bindValue('cold', ':cold', PDO::PARAM_BOOL);
+        $stm->bindValue('room', ':room', PDO::PARAM_BOOL);
+
+        $stm->execute();
+        return $stm->fetchAll();
+    }
     public function selectForLuggage()
     {
         $sql = "SELECT name, image, category FROM item
